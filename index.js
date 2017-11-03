b904c230fc649a3d48397cdbb28fdd91919fd2ba
stephan lalka <slalka@gmail.com>
2017-11-03 21:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function logShout(s){
 function logWhisper(s){
   console.log(whisper(s))
 }
+function sayHiToGrandma(s){
+  if(s.toLowerCase() === s) return "I can't hear you!";
+  if(s.toUpperCase() === s) return "YES INDEED!";
+  if(s === "I love you, Grandma.") return "I love you, too."
+}
