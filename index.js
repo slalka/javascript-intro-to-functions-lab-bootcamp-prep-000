b753e5add66ab2bde2df41a01879b10146e209fc
stephan lalka <slalka@gmail.com>
2017-11-03 21:50:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ return string.toUpperCase();
 function whisper(s){
   return s.toLowerCase();
 }
+function logShout(s){
+  console.log(shout(s))
+}
+function logWhisper(s){
+  console.log(whisper(s))
+}
